a22ae03df0f119081591184ff8248e33674dd603
Vipin <vignesh.raju@daimler.com>
2025-03-22 09:20:13 +0530

    implementing DTO

--- accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDTO.java
+++ accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDTO.java
@@ -1,5 +1,16 @@
 package com.eazybytes.accounts.dto;
 
+import lombok.Data;
+
+@Data
 public class AccountsDTO {
     
+  
+    private String accountNumber;
+
+  
+    private String accountType;
+
+ 
+    private String branchAddress;
 }
